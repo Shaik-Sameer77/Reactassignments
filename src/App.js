2f76b6272b753cd5f07221e3e8befe7cd045790d
sameer <sameer.shammu.1514@gmail.com>
2023-04-26 20:35:16 +0530

    done here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import './App.css';
-import logo from './logo.svg';
-
+import React, { useState } from "react";
+import Counter from "./component/Counter";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button>Get started...</button>
-        </a>
-      </header>
+    <div className="app-container">
+      <Counter
+        count={count}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+      />
     </div>
   );
 }
 
 export default App;
+
